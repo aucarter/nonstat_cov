78f11c3cb49265318660774cf8e97b6324398e80
aallorant <43590555+aallorant@users.noreply.github.com>
2020-03-12 10:23:05 -0700

    adding comments to the prediction script

--- analysis/03_prediction.R
+++ analysis/03_prediction.R
@@ -1,7 +1,8 @@
 rm(list=ls())
 indir <- "C:/Users/allorant/OneDrive - UW/Shared with Everyone/UW/3rdYear/Winter/STAT517/Final_project/nonstat_cov/data/"
+outdir <- "C:/Users/allorant/OneDrive - UW/Shared with Everyone/UW/3rdYear/Winter/STAT517/Final_project/output/"
 
-libs <- c('data.table','ggplot2','dplyr','tidyr','INLA')
+libs <- c('data.table','ggplot2','dplyr','INLA')
 for(l in libs){
   if(!require(l,character.only = TRUE, quietly = TRUE)){
     message( sprintf('Did not have the required package << %s >> installed. Downloading now ... ',l))
@@ -9,6 +10,10 @@ for(l in libs){
   }
   library(l, character.only = TRUE, quietly = TRUE)
 }
+
+#########################
+## Extracting the data ##
+#########################
 
 yr <- 1981
 type <- "ppt"
@@ -33,10 +38,6 @@ if( !with.infill) {
   temp[missing.ind==1] <- NA
 }
 
-
-# Set-up problem
-require(INLA)
-
 # Read station locations
 scan(paste0(indir,"METAinfo"), skip=1, what=list( "a", 1,1,1))-> look
 names(look)<-c("station.id", "lon", "lat", "elev")
@@ -52,19 +53,25 @@ count = 1
 dat <- temp
 dat <- rowSums(dat)
 
-# dataframe
+# Assembling them together in a dataframe
 totDF <- data.frame(
   y = log(dat),
   lon = look$lon,
   lat = look$lat,
   elev = look$elev/1000,
-  idx = ifelse(!is.na(dat), "est", "pred")
+  idx = ifelse(!is.na(dat), "est", "pred") # the location with missing y are given the tag prediction
 )
 
 totDF <- totDF %>%
-  mutate(elev = ifelse(elev == -500, 0, elev))
+  mutate(elev = ifelse(elev == -500, 0, elev)) # following the authors' approach we force negative elevation to be zero
 
 
+#############################
+## Estimation & Prediction ##
+#############################
+
+# We create two stacks of data, one for which both the predictor and the outcome are observed
+# another stack for which we only have the elevation and we want to predict precipitation
 
 loc <- cbind(totDF$lon[totDF$idx=="est"],totDF$lat[totDF$idx=="est"])
 
@@ -87,6 +94,7 @@ stk.est <- inla.stack(data = list(y = totDF %>% filter(idx == "est") %>% dplyr::
                                           elev = totDF %>% filter(idx == "est") %>% dplyr::select(elev)),
                                      c(inla.spde.make.index("mesh.idx",
                                                             n.spde=mesh$n))), tag="est")
+
 ## Prediction for location where log_ann_prec not observed
 loc_pred <- cbind(totDF$lon[totDF$idx=="pred"],totDF$lat[totDF$idx=="pred"])
 
@@ -100,13 +108,20 @@ stk.p <- inla.stack(data = list(y = NA),
                                      c(inla.spde.make.index("mesh.idx",
                                                             n.spde=mesh$n))), tag="pred")
 
+# We combine these two stacks in one object
+
 stk.full <- inla.stack(stk.est, stk.p)
+
 # Formula
+## We have a model with a fixed effect and a spatial effect
 formula <- y~intercept+elev+f(mesh.idx, model = spde, replicate=mesh.idx.repl)-1
 
 # Run inla
 res <- inla(formula,
-            family = "gaussian",
+            family = "gaussian", # we modelled the data as gaussian as 
+                                #in the paper even though this theoretically allows
+                                #for negative precipitations
+                                # other studies have used a gamma distribution
             data = inla.stack.data(stk.full),
             control.predictor = list(
               compute = TRUE,
@@ -115,38 +130,22 @@ res <- inla(formula,
             control.mode=list(theta=c(0.031138, -0.199624, -0.7506),
                               restart=TRUE), num.threads = 2)
 
-save(res, file = paste0(indir, "res.rda"))
-load(paste0(indir, "res.rda"))
-# 
-# library(ggplot2)
-# intercept <- res$marginals.fixed[[1]]
-# ggplot(data.frame(inla.smarginal(intercept)), aes(x, y)) +
-#   geom_line() +
-#   theme_bw()
-# marg.variance <- inla.tmarginal(function(x) 1/x,
-#                                 res$marginals.hyperpar$`Precision for the Gaussian observations`)
-# 
-# ggplot(data.frame(inla.smarginal(marg.variance)), aes(x, y)) +
-#   geom_line() +
-#   theme_bw()
-# 
-# list_marginals <- res$marginals.fitted.values
-# 
-# marginals <- data.frame(do.call(rbind, list_marginals))
-# marginals$h <- rep(names(list_marginals),
-#                           times = sapply(list_marginals, nrow))
-# res$cpu
+# save(res, file = paste0(outdir, "res.rda"))
+# load(paste0(outdir, "res.rda"))
 
 summary(res)
 
 index_est <- inla.stack.index(stk.full, tag = "est")$data
 
+# We save in separate objects
+## The mean, 2.5% and 97.5% estimated values at observed locations
 est_mean <- res$summary.fitted.values[index_est, "mean"]
 est_ll <- res$summary.fitted.values[index_est, "0.025quant"]
 est_ul <- res$summary.fitted.values[index_est, "0.975quant"]
 
 index_pred <- inla.stack.index(stk.full, tag = "pred")$data
 
+## The mean, 2.5% and 97.5% predicted values at unobserved locations
 pred_mean <- res$summary.fitted.values[index_pred, "mean"]
 pred_ll <- res$summary.fitted.values[index_pred, "0.025quant"]
 pred_ul <- res$summary.fitted.values[index_pred, "0.975quant"]
@@ -188,20 +187,20 @@ dpm <- dpm %>%
   mutate(variable = as.factor(dpm$variable))
 
 
-p <- dpm %>% 
-  filter( variable == "est_mean" | variable == "pred_mean") %>%
-  ggplot(aes(x = east, y = north, color = value)) +
-  facet_wrap(~tag, nrow = 1) +
-  geom_point(size = 1, alpha = 0.5) +
-  scale_color_gradient(
-    name = "Rain",
-    low = "blue", high = "yellow"
-  ) +
-  theme_bw()
+# p <- dpm %>% 
+#   filter( variable == "est_mean" | variable == "pred_mean") %>%
+#   ggplot(aes(x = east, y = north, color = value)) +
+#   facet_wrap(~tag, nrow = 1) +
+#   geom_point(size = 1, alpha = 0.5) +
+#   scale_color_gradient(
+#     name = "Rain",
+#     low = "blue", high = "yellow"
+#   ) +
+#   theme_bw()
+# 
+# ggsave(p, filename =  paste0(outdir, "pred.png"))
 
-ggsave(p, filename =  paste0(indir, "pred.png"))
-
-
+# Plot of the predictions
 p1 <- dpm %>% 
   filter( tag == "pred") %>%
   ggplot(aes(x = east, y = north, color = value)) +
@@ -212,7 +211,7 @@ p1 <- dpm %>%
     low = "blue", high = "yellow"
   ) +
   theme_bw()
-ggsave(p1, filename =  paste0(indir, "predAndCI.png"))
+ggsave(p1, filename =  paste0(outdir, "predAndCI.png"))
 
 p <- dpm %>% 
   filter( variable == "pred_mean") %>%
@@ -223,9 +222,9 @@ p <- dpm %>%
     name = "Rainfall",
     low = "blue", high = "orange"
   )
-dir <- "C:/Users/allorant/OneDrive - UW/Shared with Everyone/UW/3rdYear/Winter/STAT517/Final_project/code/"
-ggsave(p, paste0(dir,"predPlot.png"))
-# 
+ggsave(p, filename =paste0(outdir,"predPlot.png"))
+
+## We may also be interested in predicting the random field alone
 rang <- apply(mesh$loc[, c(1, 2)], 2, range)
 proj <- inla.mesh.projector(mesh,
                             xlim = rang[, 1], ylim = rang[, 2],
@@ -252,4 +251,4 @@ gsd <- ggplot(df, aes(x = x, y = y, fill = sd_s)) +
   coord_fixed(ratio = 1) + theme_bw()
 
 spField <- plot_grid(gmean, gsd)
-ggsave(spField, filename =  paste0(indir, "spatialField.png"))
+ggsave(spField, filename =  paste0(outdir, "spatialField.png"))
